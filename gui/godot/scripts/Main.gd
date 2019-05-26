e2d90df435b852d651c196598e70daf5dc7154cc
pguenezan <paul@guenezan.me>
2019-05-26 18:32:44 +0200

    gui: fix js export

--- gui/godot/scripts/Main.gd
+++ gui/godot/scripts/Main.gd
@@ -1,6 +1,9 @@
 extends Node
 
 func _ready():
+	if OS.has_feature('JavaScript'):
+		get_tree().change_scene("res://scenes/Replay.tscn")
+		return
 	for arg in OS.get_cmdline_args():
 		if arg.begins_with("-json="):
 			get_tree().change_scene("res://scenes/Replay.tscn")
