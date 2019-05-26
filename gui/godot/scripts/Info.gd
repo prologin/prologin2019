e63d2397f4f2dc87219c9ac5bb2a4c33d0c94632
Martin Huvelle <mhuvelle@lrde.epita.fr>
2019-05-26 15:54:43 +0200

    gui: add end event

--- gui/godot/scripts/Info.gd
+++ gui/godot/scripts/Info.gd
@@ -1,5 +1,7 @@
 extends Node
 
+var is_end = 0
+
 func _ready():
 	$SpeedSlider/Speed.text = "Vitesse: " + str(Global.speed_factor)
 	$SpeedSlider.connect("value_changed", self, "_speed_slider")
@@ -9,7 +11,22 @@ func _speed_slider(value):
 	Global.speed_factor = v
 	$SpeedSlider/Speed.text = "Vitesse: " + str(Global.speed_factor)
 	
+func _end():
+	var name_winner = ""
+	var is_end = 1
+	if $"Score 1" == $"Score 2":
+		$End.text = "Egalité\n\n Appuyer sur Esc pour quitter"
+		return
+	if $"Score 1" > $"Score 2":
+		name_winner = $"Name 1".text
+	else:
+		name_winner = $"Name 2".text
+	$End.text = name_winner + " est le plus riche!\n\n Appuyer sur Esc pour quitter"
+
 func _process(delta):
+	if is_end:
+		if Input.is_action_just_pressed("ui_escape"):
+			get_tree().quit()
 	if Input.is_action_just_pressed("ui_up"):
 		if $SpeedSlider.get_value() < $SpeedSlider.get_max():
 			$SpeedSlider.set_value($SpeedSlider.get_value() + 1)
