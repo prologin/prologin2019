9dbc56855bbc0afba33de7546a35b17f543aea2c
Martin Huvelle <mhuvelle@lrde.epita.fr>
2019-05-30 18:57:59 +0200

    gui: new arg -names=

--- gui/godot/scripts/Main.gd
+++ gui/godot/scripts/Main.gd
@@ -5,6 +5,9 @@ func _ready():
 		get_tree().change_scene("res://scenes/Replay.tscn")
 		return
 	for arg in OS.get_cmdline_args():
+		if "-names=" in arg:
+			Global.champion_names = arg.replace("-names=","")
+			print(Global.champion_names)
 		if arg.begins_with("-json="):
 			get_tree().change_scene("res://scenes/Replay.tscn")
 			return
