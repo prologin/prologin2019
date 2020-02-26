54b0fd6a7235c9dbe3999ae1d0dfd631fea73a55
Rémi Audebert <rflah0@gmail.com>
2020-02-26 22:45:26 +0100

    rules: add rules_config

--- src/entry.cc
+++ src/entry.cc
@@ -12,45 +12,53 @@
 
 #include <cstdlib>
 #include <memory>
+
 #include <rules/client-messenger.hh>
+#include <rules/config.hh>
 #include <rules/server-messenger.hh>
 #include <utils/log.hh>
 
 #include "rules.hh"
 
 // Forward decls
-namespace rules
-{
+namespace rules {
 struct Options;
-
 }
 
 static Rules* rules_;
 
-extern "C"
+extern "C" {
+
+void rules_config(rules::Config* cfg)
 {
+    cfg->name = "prologin2019";
+    cfg->player_count = 2;
+}
 
-    void rules_init(const rules::Options& opt)
-    {
-        utils::Logger::get().level() = (utils::Logger::DisplayLevel)opt.verbose;
-        rules_ = new Rules(opt);
-    }
+void rules_init(const rules::Options& opt)
+{
+    utils::Logger::get().level() = (utils::Logger::DisplayLevel)opt.verbose;
+    rules_ = new Rules(opt);
+}
 
-    void rules_result() { delete rules_; }
+void rules_result()
+{
+    delete rules_;
+}
 
-    void player_loop(rules::ClientMessenger_sptr msgr)
-    {
-        rules_->player_loop(msgr);
-    }
+void player_loop(rules::ClientMessenger_sptr msgr)
+{
+    rules_->player_loop(msgr);
+}
 
-    void server_loop(rules::ServerMessenger_sptr msgr)
-    {
-        rules_->server_loop(msgr);
-    }
+void server_loop(rules::ServerMessenger_sptr msgr)
+{
+    rules_->server_loop(msgr);
+}
 
-    void spectator_loop(rules::ClientMessenger_sptr msgr)
-    {
-        rules_->spectator_loop(msgr);
-    }
+void spectator_loop(rules::ClientMessenger_sptr msgr)
+{
+    rules_->spectator_loop(msgr);
+}
 
 } // extern "C"
